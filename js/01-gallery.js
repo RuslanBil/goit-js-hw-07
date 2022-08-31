9b016801abd6051d81b067a827168d23b72b7208
RuslanBil <bilousr0426@gmail.com>
2022-08-31 13:14:01 +0300

    homework-done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.classList.add('.gallery__item');
+const markUp = createImages(galleryItems);
+galleryRef.insertAdjacentHTML('afterbegin', markUp);
+function createImages(galleryItems) {
+  return galleryItems.reduce(
+    (acc, galleryItem) => (acc += createMarkup(galleryItem)),
+    ' ',
+  );
+}
+function createMarkup({ preview, original, description }) {
+  const itemList = `<div class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image" 
+  src='${preview}' 
+  data-source='${original}'
+  alt='${description}'></a></div>`;
+
+  return itemList;
+}
+
+galleryRef.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  instance.element().querySelector('img').src = e.target.dataset.source;
+
+  instance.show();
+  window.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  });
+});
+
+const instance = basicLightbox.create(`<img src="" />`, {
+  onShow: instance => ('onShow', instance),
+  onClose: instance => ('onClose', instance),
+});
